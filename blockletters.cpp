9b4f8ab5fdb80eb8ae542ba224440b92ab7c5ea0
Amy <39568291+amyjblain@users.noreply.github.com>
2019-07-25 11:09:35 -0400

    Add files via upload

---
+++ blockletters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	std::cout<<"   A       B B B B  \n";
+	std::cout<<" A   A     B       B\n";
+	std::cout<<"A     A    B       B\n";
+	std::cout<<"A A A A    B B B B  \n";
+	std::cout<<"A     A    B       B\n";
+	std::cout<<"A     A    B       B\n";
+	std::cout<<"A     A    B B B B  \n";
+	return 0;
+}
